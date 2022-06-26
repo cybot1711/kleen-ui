c99cd94e77f8a5f63e7b97d59b92d6f7527ba5fa
cybot1711 <jahil.khalfe@gmail.com>
2022-06-26 16:45:52 +0200

    Enable MUI theme for storybook.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,21 @@
+import React from "react";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import { StyledEngineProvider, ThemeProvider } from "@mui/material/styles";
+import theme from "../src/theme";
+
+export const decorators = [
+  Story =>
+    (
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Story />
+        </ThemeProvider>
+      </StyledEngineProvider>
+    )
+];
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+  actions: { argTypesRegex: "^(on|handle)[A-Z].*" }
+};
