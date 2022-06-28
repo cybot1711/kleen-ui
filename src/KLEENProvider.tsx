0e24ee1784fa683d2bd6292ef622375808d26bca
cybot1711 <jahil.khalfe@gmail.com>
2022-06-28 09:37:00 +0200

    Update babel config

--- src/KLEENProvider.tsx
+++ src/KLEENProvider.tsx
@@ -1,4 +1,4 @@
-import React, { type FC, type ReactNode } from 'react'
+import React, { type FC, type ReactNode, StrictMode } from 'react'
 import { CssBaseline, StyledEngineProvider, ThemeProvider } from '@mui/material'
 import theme from './theme'
 
@@ -6,12 +6,12 @@ import theme from './theme'
  * Custom provider for providing theme to your app
  */
 export const KlEENProvider: FC<{ children: ReactNode | undefined }> = ({ children }) => (
-  <React.StrictMode>
+  <StrictMode>
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         {children}
       </ThemeProvider>
     </StyledEngineProvider>
-  </React.StrictMode>
+  </StrictMode>
 )
